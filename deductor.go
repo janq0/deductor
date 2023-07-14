d7bc1e8474f6fc8fae28e66084e6706731e945be
Jan Konzal <janq0q0@gmail.com>
2023-07-14 11:52:50 +0200

    Add tokens function

--- deductor.go
+++ deductor.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"regexp"
+	"strings"
+)
 
-func main() {
-	fmt.Println("Hello, world.")
+type Parser struct {
+	matchDelimiters regexp.Regexp
+	// OperatorByString map[string]Operator
 }
+
+func (t *Parser) tokens(line string) []string {
+	delimiters := t.matchDelimiters.FindAllString(line, -1)
+	nondelimiters := t.matchDelimiters.Split(line, -1)
+	result := make([]string, 0, len(delimiters)+len(nondelimiters))
+	result = appendTrimSpaceIfNonempty(result, nondelimiters[0])
+	for i := 0; i < len(delimiters); i++ {
+		result = appendTrimSpaceIfNonempty(result, delimiters[i])
+		result = appendTrimSpaceIfNonempty(result, nondelimiters[i+1])
+	}
+	return result
+}
+
+func appendTrimSpaceIfNonempty(slice []string, item string) []string {
+	if trim := strings.TrimSpace(item); len(trim) > 0 {
+		return append(slice, trim)
+	}
+	return slice
+}
+
+func main() {}
---
+++ deductor_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"regexp"
+	"testing"
+)
+
+var simpleDelimParser Parser = Parser{*regexp.MustCompile(".|~ | & ")}
+
+func TestTokensOnEmptyLine(t *testing.T) {
+	got := simpleDelimParser.tokens("")
+	if len(got) > 0 {
+		t.Errorf(`singleDelimParser.tokens("") = %v; want []`, got)
+	}
+}
+
+func TestTokensOnSingleDelimiter(t *testing.T) {
+	got := simpleDelimParser.tokens(" & ")
+	if len(got) != 1 || got[0] != "&" {
+		t.Errorf(`singleDelimParser.tokens(" & ") = "%v"; want [&]`, got)
+	}
+}
+
+func TestTokensOnSingleNondelimiter(t *testing.T) {
+	got := simpleDelimParser.tokens("a")
+	if len(got) != 1 || got[0] != "a" {
+		t.Errorf(`singleDelimParser.tokens("a") = "%v"; want [a]`, got)
+	}
+}
+
+func TestTokensOnComplexExpression(t *testing.T) {
+	got := simpleDelimParser.tokens(".a & b. & ~ c")
+	if len(got) != 8 || got[0] != "." || got[1] != "a" || got[2] != "&" || got[3] != "b" || got[4] != "." || got[5] != "&" || got[6] != "~" || got[7] != "c" {
+		t.Errorf(`singleDelimParser.tokens("a") = "%v"; want [. a & b . & ~ c]`, got)
+	}
+}
